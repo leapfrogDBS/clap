32ee1de2eaa6bebe480dbbca04c6bd4f1e4634dd
David McDonagh <david@leapfrogdbs.co.uk>
2024-07-12 14:53:06 +0100

    Updates

--- develop/front-page.php
+++ develop/front-page.php
@@ -48,44 +48,47 @@
 
         ?>    
             <div class="section slide"  id="slide-<?php the_ID(); ?>"  data-post-id="<?php the_ID(); ?>" data-post-url="<?php echo esc_url($post_url); ?>">
-                <video src="<?php echo esc_url($video_preview_url); ?>" loop muted playsinline <?= $autoplay ?> class="default-video h-full w-full object-cover z-40 relative"></video>
+                
 
                 <div class="slide-info flex items-end justify-between gap-x-6 container text-white z-50">
                     
-                        <div class="project-details max-w-2xl flex flex-col items-start gap-y-6">
-                            <div class="flex flex-col sm:flex-row items-start sm:items-center gap-6">
-                                <img src="<?= $logo['url']; ?>" alt="<?= $logo['alt']; ?>" class="h-16 w-auto">
-                                <?php if($show_title) : ?>
-                                    <h2><?= the_title(); ?></h2>
-                                <?php endif; ?>
-                            </div>
-                            
-                            <p class="text-lg md:text-2xl font-medium"><?= $caption; ?></p>
-                            <div>
-                                <a href="<?= the_permalink(); ?>" class="uppercase text-black leading-none font-semibold bg-white py-4 px-6 rounded-full inline-block hover:text-white hover:bg-black transition-all ease-in-out duration-500">Watch</a>
-                            </div>
+                    <div class="project-details max-w-2xl flex flex-col items-start gap-y-6">
+                        <div class="flex flex-col sm:flex-row items-start sm:items-center gap-6">
+                            <img src="<?= $logo['url']; ?>" alt="<?= $logo['alt']; ?>" class="h-16 w-auto">
+                            <?php if($show_title) : ?>
+                                <h2><?= the_title(); ?></h2>
+                            <?php endif; ?>
                         </div>
-                        <div class="side-bar z-50">
-                            <div class="side-icon like-btn" data-liked="false">
-                                <i class="fa fa-heart-o like-icon"></i>
-                                <p class="like-number"><?php echo esc_html($like_number); ?></p>
-                            </div>
-                            <div class="side-icon share-btn">
-                                <i class="fa fa-share share-icon"></i>
-                            </div>
+                        
+                        <p class="text-lg md:text-2xl font-medium"><?= $caption; ?></p>
+                        <div>
+                            <a href="<?= the_permalink(); ?>" class="uppercase text-black leading-none font-semibold bg-white py-4 px-6 rounded-full inline-block hover:text-white hover:bg-black transition-all ease-in-out duration-500">Watch</a>
                         </div>
                     </div>
-
-                     <div class="share-options z-50">
-                        <button class="share-option" data-share-type="facebook"><i class="fa fa-facebook"></i></button>
-                        <button class="share-option" data-share-type="twitter"><i class="fa-brands fa-x-twitter"></i></button>
-                        <button class="share-option" data-share-type="email"><i class="fa fa-envelope"></i></button>
-                        <button class="share-option" data-share-type="copy"><i class="fa fa-link"></i></button>
+                    <div class="side-bar z-50">
+                        <div class="side-icon like-btn" data-liked="false">
+                            <i class="fa fa-heart-o like-icon"></i>
+                            <p class="like-number"><?php echo esc_html($like_number); ?></p>
+                        </div>
+                        <div class="side-icon share-btn">
+                            <i class="fa fa-share share-icon"></i>
+                        </div>
                     </div>
+                </div>
 
-                    <div class="scroll-down absolute bottom-0 w-full flex justify-center z-50">
-                        <i class="fa-solid fa-chevron-down text-4xl text-white p-6 scroll-icon cursor-pointer"></i>
-                    </div>
+                <div class="share-options z-50">
+                    <button class="share-option" data-share-type="facebook"><i class="fa fa-facebook"></i></button>
+                    <button class="share-option" data-share-type="twitter"><i class="fa-brands fa-x-twitter"></i></button>
+                    <button class="share-option" data-share-type="email"><i class="fa fa-envelope"></i></button>
+                    <button class="share-option" data-share-type="copy"><i class="fa fa-link"></i></button>
+                </div>
+
+                <div class="scroll-down absolute bottom-0 w-full flex justify-center z-50">
+                    <i class="fa-solid fa-chevron-down text-4xl text-white p-6 scroll-icon cursor-pointer"></i>
+                </div>
+
+                <video src="<?php echo esc_url($video_preview_url); ?>" loop muted playsinline <?= $autoplay ?> class="default-video h-full w-full object-cover z-40 absolute"></video>
+
             </div>
    
         <?php
